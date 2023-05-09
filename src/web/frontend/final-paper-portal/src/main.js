27bf609903d9d5bf6c8e64cb9d91a7288d2fc6e4
Masimo Licul <mlicul@symmetria-consulting.hr>
2023-05-10 00:28:36 +0200

    Needs more styling

--- src/web/frontend/final-paper-portal/src/main.js
+++ src/web/frontend/final-paper-portal/src/main.js
@@ -15,6 +15,7 @@ import Checkbox from 'primevue/checkbox'
 import InputText from 'primevue/inputtext'
 import Dropdown from 'primevue/dropdown'
 import RadioButton from 'primevue/radiobutton'
+import Dialog from 'primevue/dialog'
 import './assets/styles.scss'
 
 const app = createApp(App);
@@ -35,5 +36,6 @@ app.component('Sidebar', Sidebar);
 app.component('TabMenu', TabMenu);
 app.component('Checkbox', Checkbox);
 app.component('InputText', InputText);
+app.component('Dialog', Dialog);
 
 app.mount('#app');
